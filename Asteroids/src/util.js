c5e2f558c3d13b514dac3424b5f9796f07b6bacd
yakzaetz <yakzaetz@gmail.com>
2022-01-31 15:34:08 -0500

    completed inheritance module

--- Asteroids/src/util.js
+++ Asteroids/src/util.js
@@ -1,6 +1,13 @@
 const Util = {
     inherits(childClass, parentClass) {
         //...
+        // childClass.prototype = Object.create(parentClass.prototype);
+        // childClass.prototype.constructor = childClass;
+
+        function Surrogate(){};
+        Surrogate.prototype = parentClass.prototype;
+        childClass.prototype = new Surrogate();
+        childClass.prototype.constructor = childClass;
     }
 };
 
